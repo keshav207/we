85f66e3023f234d6f10742731fea60ffb9d17f38
keshav <keshavarg2269@gmail.comgit config --global user.name keshavgit commitomit --global commitgit config --global user.email keshavarg2269@gmail.com>
2021-02-23 17:04:20 +0530

    just commit

---
+++ script.py
@@ -0,0 +1,9 @@
+#basic
+print("hello")#it print hello on output.
+
+ #making variables and strings
+selmon=hii     #In it the variable is Selmon and string is hii
+print (selmon) #in it you will see the output hii because you make variable
+                
+
+
